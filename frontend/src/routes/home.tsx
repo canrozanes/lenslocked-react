23008b67e9f3e756152f9521d7d72be4e0597735
Can Rozanes <37912128+canrozanes@users.noreply.github.com>
2023-07-24 10:00:17 -0400

    add docker, remove all react code

--- frontend/src/routes/home.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="px-6">
-      <h1 className="py-4 text-4xl semibold tracking-tight">
-        Welcome to my awesome site!
-      </h1>
-    </div>
-  );
-}
