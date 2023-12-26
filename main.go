8a049996ff2093a277aaa23319e5eb485bd65bbd
Can Rozanes <can@squareup.com>
2023-12-26 11:15:26 -0500

    section 22: image

--- main.go
+++ main.go
@@ -197,6 +197,10 @@ func main() {
 			r.Get("/{id}/edit", galleriesC.Edit)
 			r.Post("/{id}", galleriesC.Update)
 			r.Post("/{id}/delete", galleriesC.Delete)
+			r.Get("/{id}", galleriesC.Show)
+			r.Get("/{id}/images/{filename}", galleriesC.Image)
+			r.Post("/{id}/images/{filename}/delete", galleriesC.DeleteImage)
+			r.Post("/{id}/images", galleriesC.UploadImage)
 		})
 	})
 
