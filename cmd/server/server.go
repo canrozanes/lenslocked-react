130bcb9e61cace451c7ab111ea7ea671692d0e80
Can Rozanes <can@squareup.com>
2023-12-29 20:28:21 -0500

    section 23: preparing for production

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -12,7 +12,6 @@ import (
 	"github.com/canrozanes/lenslocked/templates"
 	"github.com/canrozanes/lenslocked/views"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/gorilla/csrf"
 	"github.com/joho/godotenv"
 )
@@ -30,76 +29,85 @@ type config struct {
 }
 
 func loadEnvConfig() (config, error) {
+	fmt.Println("Loading env configs...")
 	var cfg config
 	err := godotenv.Load()
 	if err != nil {
 		return cfg, err
 	}
+	cfg.PSQL = models.PostgresConfig{
+		Host:     os.Getenv("PSQL_HOST"),
+		Port:     os.Getenv("PSQL_PORT"),
+		User:     os.Getenv("PSQL_USER"),
+		Password: os.Getenv("PSQL_PASSWORD"),
+		Database: os.Getenv("PSQL_DATABASE"),
+		SSLMode:  os.Getenv("PSQL_SSLMODE"),
+	}
+	if cfg.PSQL.Host == "" && cfg.PSQL.Port == "" {
+		return cfg, fmt.Errorf("No PSQL config provided.")
+	}
 
-	// TODO: PSQL
-	cfg.PSQL = models.DefaultPostgresConfig()
-
-	// TODO: SMTP
 	cfg.SMTP.Host = os.Getenv("SMTP_HOST")
 	portStr := os.Getenv("SMTP_PORT")
 	cfg.SMTP.Port, err = strconv.Atoi(portStr)
 	if err != nil {
-		panic(err)
+		return cfg, err
 	}
 	cfg.SMTP.Username = os.Getenv("SMTP_USERNAME")
 	cfg.SMTP.Password = os.Getenv("SMTP_PASSWORD")
 
-	// TODO: CSRF
-	cfg.CSRF.Key = "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
-	cfg.CSRF.Secure = false
+	cfg.CSRF.Key = os.Getenv("CSRF_KEY")
+	cfg.CSRF.Secure = os.Getenv("CSRF_SECURE") == "true"
 
-	// TODO: Server
-	cfg.Server.Address = ":3000"
+	cfg.Server.Address = os.Getenv("SERVER_ADDRESS")
 
 	return cfg, nil
 }
 
 func main() {
-
-	// Setup a database connection
+	fmt.Println("Running main...")
 	cfg, err := loadEnvConfig()
 	if err != nil {
 		panic(err)
 	}
-	db, err := models.Open(cfg.PSQL)
-
+	fmt.Println("loaded env configs")
+	err = run(cfg)
 	if err != nil {
 		panic(err)
 	}
+}
 
+func run(cfg config) error {
+	// Setup the database
+	fmt.Println("Connecting to the database...")
+	fmt.Println(cfg.PSQL.Host)
+	db, err := models.Open(cfg.PSQL)
+	if err != nil {
+		return err
+	}
 	defer db.Close()
 
-	// migrations are the folder
 	err = models.MigrateFS(db, migrations.FS, ".")
-
 	if err != nil {
-		panic(err)
+		return err
 	}
 
+	// Setup services
 	userService := &models.UserService{
 		DB: db,
 	}
-
 	sessionService := &models.SessionService{
-		DB:            db,
-		BytesPerToken: 32,
+		DB: db,
 	}
-
 	pwResetService := &models.PasswordResetService{
 		DB: db,
 	}
-
+	emailService := models.NewEmailService(cfg.SMTP)
 	galleryService := &models.GalleryService{
 		DB: db,
 	}
 
-	emailService := models.NewEmailService(cfg.SMTP)
-
+	// Setup middleware
 	umw := controllers.UserMiddleware{
 		SessionService: sessionService,
 	}
@@ -110,37 +118,33 @@ func main() {
 		csrf.Path("/"),
 	)
 
-	// Setup our router
-	r := chi.NewRouter()
-	// These middleware are used everywhere.
-	r.Use(middleware.Logger)
-	r.Use(csrfMw)
-	r.Use(umw.SetUser)
-
+	// Setup controllers
 	usersC := controllers.Users{
 		UserService:          userService,
 		SessionService:       sessionService,
 		PasswordResetService: pwResetService,
 		EmailService:         emailService,
 	}
-
-	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
-	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
+	usersC.Templates.New = views.Must(views.ParseFS(
+		templates.FS,
+		"signup.gohtml", "tailwind.gohtml",
+	))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(
+		templates.FS,
+		"signin.gohtml", "tailwind.gohtml",
+	))
 	usersC.Templates.ForgotPassword = views.Must(views.ParseFS(
 		templates.FS,
 		"forgot-pw.gohtml", "tailwind.gohtml",
 	))
-
 	usersC.Templates.CheckYourEmail = views.Must(views.ParseFS(
 		templates.FS,
 		"check-your-email.gohtml", "tailwind.gohtml",
 	))
-
 	usersC.Templates.ResetPassword = views.Must(views.ParseFS(
 		templates.FS,
 		"reset-pw.gohtml", "tailwind.gohtml",
 	))
-
 	galleriesC := controllers.Galleries{
 		GalleryService: galleryService,
 	}
@@ -148,70 +152,72 @@ func main() {
 		templates.FS,
 		"galleries/new.gohtml", "tailwind.gohtml",
 	))
-
 	galleriesC.Templates.Edit = views.Must(views.ParseFS(
 		templates.FS,
 		"galleries/edit.gohtml", "tailwind.gohtml",
 	))
-
 	galleriesC.Templates.Index = views.Must(views.ParseFS(
 		templates.FS,
 		"galleries/index.gohtml", "tailwind.gohtml",
 	))
-
 	galleriesC.Templates.Show = views.Must(views.ParseFS(
 		templates.FS,
 		"galleries/show.gohtml", "tailwind.gohtml",
 	))
 
-	r.Get("/", controllers.StaticHandler(views.Must(
-		views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
-
-	r.Get("/contact", controllers.StaticHandler(views.Must(
-		views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
-
-	r.Get("/faq", controllers.FAQ(
-		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
-
+	// Setup our router and routes
+	r := chi.NewRouter()
+	r.Use(csrfMw)
+	r.Use(umw.SetUser)
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"home.gohtml", "tailwind.gohtml",
+	))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(
+		templates.FS,
+		"contact.gohtml", "tailwind.gohtml",
+	))))
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(
+		templates.FS,
+		"faq.gohtml", "tailwind.gohtml",
+	))))
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
 	r.Get("/signin", usersC.SignIn)
 	r.Post("/signin", usersC.ProcessSignIn)
-	r.Route("/users/me", func(r chi.Router) {
-		r.Use(umw.RequireUser)
-		r.Get("/", usersC.CurrentUser)
-	})
 	r.Post("/signout", usersC.ProcessSignOut)
 	r.Get("/forgot-pw", usersC.ForgotPassword)
 	r.Post("/forgot-pw", usersC.ProcessForgotPassword)
 	r.Get("/reset-pw", usersC.ResetPassword)
 	r.Post("/reset-pw", usersC.ProcessResetPassword)
-
+	r.Route("/users/me", func(r chi.Router) {
+		r.Use(umw.RequireUser)
+		r.Get("/", usersC.CurrentUser)
+	})
 	r.Route("/galleries", func(r chi.Router) {
 		r.Get("/{id}", galleriesC.Show)
+		r.Get("/{id}/images/{filename}", galleriesC.Image)
 		r.Group(func(r chi.Router) {
 			r.Use(umw.RequireUser)
-			r.Get("/new", galleriesC.New)
 			r.Get("/", galleriesC.Index)
+			r.Get("/new", galleriesC.New)
 			r.Post("/", galleriesC.Create)
 			r.Get("/{id}/edit", galleriesC.Edit)
 			r.Post("/{id}", galleriesC.Update)
 			r.Post("/{id}/delete", galleriesC.Delete)
-			r.Get("/{id}", galleriesC.Show)
-			r.Get("/{id}/images/{filename}", galleriesC.Image)
-			r.Post("/{id}/images/{filename}/delete", galleriesC.DeleteImage)
 			r.Post("/{id}/images", galleriesC.UploadImage)
+			r.Post("/{id}/images/{filename}/delete", galleriesC.DeleteImage)
 		})
 	})
+	assetsHandler := http.FileServer(http.Dir("assets"))
+	r.Get("/assets/*", http.StripPrefix("/assets", assetsHandler).ServeHTTP)
 
+	// r.Get("/users/me", usersC.CurrentUser)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 
+	// Start the server
 	fmt.Printf("Starting the server on %s...\n", cfg.Server.Address)
-	err = http.ListenAndServe(":3000", r)
-
-	if err != nil {
-		panic(err)
-	}
+	return http.ListenAndServe(cfg.Server.Address, r)
 }
